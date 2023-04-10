ea7588d1a4c7adea18720eb8ca731bef639dffc0
man2k <mankarmakar4@gmail.com>
2023-04-10 17:01:50 +0530

    fixed styles and added userChoice for enc. algorithm

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,16 +1,20 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
-
+import Select from 'react-select'
+import obj from './ChipherList'
+// console.log(obj)
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [UserChoice, setChoice] = useState('')
 
   return (
-    <div className="App">
+    <div class="App">
       <form id="UploadForm" action="http://localhost:3000/upload" method="POST" enctype="multipart/form-data">
         <input type="file" name="file" />
         <button class="file-upload-button" type="submit">Upload</button>
       </form>
+      <Select options={obj} className="react-select-container" classNamePrefix="react-select" onChange={(choice)=>setChoice(choice.value)}/>
+      {console.log(UserChoice)}
       <form class="form-container" action="http://localhost:3000/encrypt" method="GET">
         <button class="button" type="encrypt">Encrypt</button>
       </form>
