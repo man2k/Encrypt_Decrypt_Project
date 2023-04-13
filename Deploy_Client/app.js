56231c02db845af24d7d2d958a15d775e161c2e7
man2k <mankarmakar4@gmail.com>
2023-04-14 04:32:06 +0530

    cyclic

--- Deploy_Client/app.js
+++ Deploy_Client/app.js
@@ -14,8 +14,6 @@ const decryptFile = require("./controllers/decryptFile");
 
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static("./public"));
 
 // #############################################################################
